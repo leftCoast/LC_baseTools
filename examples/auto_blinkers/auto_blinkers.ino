43b154aeb1f31ea32df4f3e109966cd3b0961653
Jim Lee <gitHub@leftcoast.biz>
2021-04-20 00:46:21 -0700

    Put the Auto blinker example back.

--- examples/auto_blinkers/auto_blinkers.ino
+++ examples/auto_blinkers/auto_blinkers.ino
@@ -1,3 +1,29 @@
+// Auto blinker switch example.
+//
+// The auto blinker question comes up regulary in the Arduino forum so
+// I figured an example of that would be just the ticket. This uses two
+// binker objects for the blinking LEDs and three mechButton objects for
+// the three buttons. A left, right and Hazard button. The first press
+// of a button, say right, turns on that blinker. The second turns it
+// back off. If another button is pressed, say left, then the code just
+// switches from right to left. So each button is one click on, second
+// click off.
+//
+// The buttons should be wired one side to the pin number choosen and
+// the other to ground. the mechButton object will do the debounceing
+// for you as well as call your callback routines when clicked. The
+// blinker objects take care of the blinking. All these behind the
+// scenes functions happen when you call idle() in your loop function.
+//
+// You can add more stuff to your loop() function if you want. But DO
+// NOT USE delay(). No fear though, this stuff comes with sleep() wich
+// acts on the main loop() just like delay(), but lets the background
+// stuff keep running.
+//
+// Have fun!
+// jim lee
+
+
 
 #include <blinker.h>
 #include <mechButton.h>
@@ -13,10 +39,10 @@
 enum blinkComs { blinkOff, blinkLeft, blinkRight, blinkHazard };  // Set up our list of states.
 blinkComs   currentState;                                         // A variable to save our current state.
 blinker     leftBlinker(BLINK_L_PIN, BLINK_LIT, BLINK_PERIOD);    // The left blinker object. A "fire and forget" blinker object.
-blinker     rightBlinker(BLINK_L_PIN, BLINK_LIT, BLINK_PERIOD);   // The right blinker object. Same as left, different pin.
+blinker     rightBlinker(BLINK_R_PIN, BLINK_LIT, BLINK_PERIOD);   // The right blinker object. Same as left, different pin.
 mechButton  lButton(BTN_L_PIN);                                   // The left blinker button. Another "fire and forget" object.
 mechButton  rButton(BTN_R_PIN);                                   // The right blinker button. Same as left, different pin.
-mechButton  hButton(BTN_R_PIN);                                   // And your hazard button. Wouldn't be complete without them.
+mechButton  hButton(BTN_H_PIN);                                   // And your hazard button. Wouldn't be complete without them.
 
 
 // Standard setup.
@@ -33,7 +59,7 @@ void setup() {
 void clickLBtn(void) {
 
   if (!lButton.trueFalse()) {         // If the button has been grounded..
-    if (currentState==blinkLeft) {    // If we are already doing this mode..
+    if (currentState == blinkLeft) {  // If we are already doing this mode..
       setBlinkers(blinkOff);          // We just shut off everything.
     } else {                          // Else, doing something else..
       setBlinkers(blinkLeft);         // We switch to our mode.
@@ -46,7 +72,7 @@ void clickLBtn(void) {
 void clickRBtn(void) {
 
   if (!rButton.trueFalse()) {         // If the button has been grounded..
-    if (currentState==blinkRight) {   // If we are already doing this mode..
+    if (currentState == blinkRight) { // If we are already doing this mode..
       setBlinkers(blinkOff);          // We just shut off everything.
     } else {                          // Else, doing something else..
       setBlinkers(blinkRight);        // We switch to our mode.
@@ -58,13 +84,13 @@ void clickRBtn(void) {
 // When the HAZARD button is clicked, this is called..
 void clickHBtn(void) {
 
-  if (!hButton.trueFalse()) {         // If the button has been grounded..]
-'.[[]    if (currentState==blinkHazard) {    // If we are already doing this mode..
-      setBlinkers(blinkOff);            // We just shut off everything.
-    } else {                            // Else, doing something else..
-      setBlinkers(blinkHazard);         // We switch to our mode.
-    }
-  }
+   if (!hButton.trueFalse()) {            // If the button has been grounded..]
+      if (currentState == blinkHazard) {  // If we are already doing this mode..
+         setBlinkers(blinkOff);           // We just shut off everything.
+      } else {                            // Else, doing something else..
+         setBlinkers(blinkHazard);        // We switch to our mode.
+      }
+   }
 }
 
 
@@ -85,6 +111,8 @@ void setBlinkers(blinkComs theBlinkCom) {
       rightBlinker.setOnOff(true);    // Right blinker on.
       break;
     case blinkHazard  :               // blinkRight
+      leftBlinker.setOnOff(false);    // Left blinker off. (Both off to synk them up)
+      rightBlinker.setOnOff(false);   // Right blinker off.
       leftBlinker.setOnOff(true);     // Left blinker on.
       rightBlinker.setOnOff(true);    // Right blinker on.
       break;
@@ -95,4 +123,6 @@ void setBlinkers(blinkComs theBlinkCom) {
 
 // Everything is run behind the scenes so all loop()
 // needs to do is call idle() to let it all run.
-void loop() { idle(); }
+void loop() {
+  idle();
+}
