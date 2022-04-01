e2951232a2901da66f6780c61c394bfa1fe0163c
MaderDash <32471649+MaderDash@users.noreply.github.com>
2022-04-01 00:34:31 -0500

    Fixed Typo

--- examples/blink_basic_example/blink_basic_example.ino
+++ examples/blink_basic_example/blink_basic_example.ino
@@ -8,7 +8,7 @@ blinker blinkingLed(13, 200, 1000);
 // EXAMPLE::::: nameOfblinker(PIN, ON_TIME, TIME_FOR_BLINKS);
 
 void setup() {
-  blinkingLed.setOnOff(true);    // If this is wet to "true", the blinker will run, if "false"it will stop.
+  blinkingLed.setOnOff(true);    // If this is set to "true", the blinker will run, if "false"it will stop.
 
 }
 
