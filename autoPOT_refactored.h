b3862634fef1dfabbdcab3c26635e832991acd4f
maderdash <supperslash@gmail.com>
2024-07-02 02:32:54 -0500

    Added two files. Both refactors drafts of autopot. To add the averaging features.

---
+++ autoPOT_refactored.h
@@ -0,0 +1,34 @@
+
+#include <autoPOT.h>
+
+autoPOT::autoPOT(int inPin) {
+    callback = NULL;
+    pinNum = inPin;
+    value = -1;
+}
+
+autoPOT::~autoPOT() {}
+
+void autoPOT::setCallback(void(*funct)(int)) {
+    callback = funct;
+    hookup();
+}
+
+void autoPOT::idle() {
+    int newVal;
+    if (callback) {
+        newVal = analogRead(pinNum);
+        if (newVal != value) {
+            value = newVal;
+            callback(value);
+        }
+    }
+}
+
+int autoPOT::getAverage(int numSamples) {
+    int sum = 0;
+    for (int i = 0; i < numSamples; i++) {
+        sum += analogRead(pinNum);
+    }
+    return sum / numSamples;
+}
